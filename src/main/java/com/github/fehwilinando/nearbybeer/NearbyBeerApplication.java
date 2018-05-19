0ea33527d5a4ee43171d9ed4247862e0e5dfc346
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-05-19 20:24:12 -0300

    Setup project

---
+++ src/main/java/com/github/fehwilinando/nearbybeer/NearbyBeerApplication.java
@@ -0,0 +1,12 @@
+package com.github.fehwilinando.nearbybeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbyBeerApplication.class, args);
+	}
+}
